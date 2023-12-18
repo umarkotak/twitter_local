1303741096d5dc8b3a27ea4097b9e633076b9ba7
umar.ramadhana <umar.ramadhana@everywhere.id>
2023-12-19 05:34:44 +0700

    migration for create followers table

---
+++ db/migrations/000003_create_followers.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE followers (
+  id BIGSERIAL PRIMARY KEY,
+
+  user_id UUID NOT NULL,
+  following_user_id UUID NOT NULL,
+
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+ALTER TABLE followers ADD CONSTRAINT fk_followers_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE followers ADD CONSTRAINT fk_followers_following_user_id FOREIGN KEY (following_user_id) REFERENCES users(id);
